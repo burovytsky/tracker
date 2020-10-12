bfee9b69b433ff162fbf5997fd914cfa12079f43
burovytsky <esfero1505@gmail.com>
2020-10-12 19:16:32 +0300

    2. Сканиварование проекта [#330053]

--- src/main/java/di/ConsoleInput.java
+++ src/main/java/di/ConsoleInput.java
@@ -1,9 +1,11 @@
 package di;
 
+import org.springframework.stereotype.Component;
 import tracker.Input;
 
 import java.util.Scanner;
 
+@Component
 public class ConsoleInput implements Input {
     private Scanner scanner = new Scanner(System.in);
     @Override
